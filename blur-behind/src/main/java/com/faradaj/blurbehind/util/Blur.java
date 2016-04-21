8ada1a55c3b30272ac979708254490db05884cda
José Carlos Andreu Galán <jose.andreu@maedanstudio.com>
2016-04-21 11:37:41 +0200

    Solve missing imports

--- blur-behind/src/main/java/com/faradaj/blurbehind/util/Blur.java
+++ blur-behind/src/main/java/com/faradaj/blurbehind/util/Blur.java
@@ -2,6 +2,7 @@ package com.faradaj.blurbehind.util;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.os.Build.VERSION;
 import android.renderscript.Allocation;
